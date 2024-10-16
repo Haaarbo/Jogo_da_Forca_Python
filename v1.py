3b34e868bc77fd00e0b1a099af707b4a74cb2c72
Haaarbo <abrahao0707@gmail.com>
2024-10-16 02:39:28 -0400

    fim de v1
    corrigido a parte do carregamento do arquivo json para uma lista, trocado o remove pelo del para eliminar uma parte especifica da lista e organizado alguns fors para que seja executado de forma fluida

--- v1.py
+++ v1.py
@@ -1,6 +1,6 @@
 from os import system, name
 import random
-from json import dumps
+from json import load
 
 #limpa tela
 def limpa_tela() -> None:
@@ -11,6 +11,7 @@ def limpa_tela() -> None:
 #imprime a palavra recebida letra a letra com espacos
 def imprime_palavra(p) -> None:
     for letra in p: print(f'{letra}', end=' ')
+    print()
 
 
 #substitui a impressao 
@@ -18,8 +19,11 @@ def preenche_palavra(letra_substituida: str, lista_underlines: str, palavra_refe
     for k, l in enumerate(palavra_referencia):
         #SE NUMA DETERMINADA POSICAO A LETRA QUE EU QUISER TROCAR FOR A MESMA QUE A QUE ESTA PASSANDO, ENTAO REALIZA A TROCA NESTA PÓSICAO
         if l == letra_substituida:
-            lista_underlines.remove(k)
+            del lista_underlines[k]
             lista_underlines.insert(k, letra_substituida)
+    
+    #se a uniao de todas as letras da lista for igual a palavra
+    return True if palavra_referencia == ''.join(lista_underlines) else False
 
 
 #inicia game
@@ -30,18 +34,16 @@ def game() -> None:
     print('Adivinhe a palavra abaixo:\n')
     print('-'*20)
 
-    #palavras que podem ser sorteadas
-    palavras: str = dumps('words.json')
 
-    #sorteio da palavra
-    palavra_sorteada: str = random.choice(palavras)
+    #acesso ao arquivo com a base das palavras e sorteio da palavra
+    palavra_sorteada = random.choice(load(open('words.json', 'r', encoding='utf-8')))
     print(palavra_sorteada)
     #lista de acertos
     letras_acertadas = list()
 
     #a palavra sorteada tem um certo tamanho, então vamos cobrir a lista de letras acertadas de _ e a cada acerto trocamos pela letra certa
-    for _ in  range(len(palavra_sorteada)):
-        letras_acertadas.append('_')
+    letras_acertadas = ['_' for _ in range(len(palavra_sorteada))]
+
     
     #cria lista vazia para armazenar letras adivinhadas
     letras_tentadas = list()
@@ -51,19 +53,26 @@ def game() -> None:
     #variável para verificar se o jogador venceu
     win = False
 
+
     #enquanto nao acertar ou acabar as chances, segue
     for tentativas in range(chances, 0, -1):
-        print(f'Chances: {tentativas}\nLetras erradas: {letras_tentadas}')
+        print(f'Chances: {tentativas}\nLetras tentadas: {letras_tentadas}')
+        imprime_palavra(letras_acertadas)
+        
         entrada = input('Digite uma letra: ')
         
         if entrada in palavra_sorteada:
             win = preenche_palavra(entrada, letras_acertadas, palavra_sorteada)
-            
-
+        if win:
+                break
         else:
             letras_tentadas.append(entrada)
 
-
+    #mensagem final
+    if win:
+        print(f'Parabens! Voce acertou a palavra: {palavra_sorteada}')
+    else:
+        print(f'Ops! Abacabaram suas tentativas! A palavra era {palavra_sorteada}.')
 
 #run
 game()
